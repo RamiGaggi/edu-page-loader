09f2a0ffdb40c3f5952b0539d8ee556a27784f63
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-06-08 10:02:41 +0300

    Refactor tests for page-loader

--- tests/test_download_engine.py
+++ tests/test_download_engine.py
@@ -8,7 +8,10 @@ import requests
 import requests_mock
 from page_loader.download_engine import download
 
-TEST_ADRESS = 'http://test1/test2/tes&t3/test4/tes%t5.com'
+TEST_ADRESS = 'http://mytest.com/caramba123'
+TEST_CSS = 'http://mytest.com/caramba123/assets/application.css'
+TEST_PNG = 'http://mytest.com/caramba123/assets/nodejs.png'
+TEST_HTML = 'http://mytest.com/caramba123/assets/courses.html'
 
 
 @pytest.fixture
@@ -25,27 +28,100 @@ def expected_page():
         return html.read()
 
 
-def test_download_cwd(source_page, expected_page):
+@pytest.fixture
+def resource_page_css():
+    """Return css in str format."""
+    with open('tests/fixtures/assets/application.css', 'r') as css:
+        return css.read()
+
+
+@pytest.fixture
+def resource_page_png():
+    """Return png in str format."""
+    with open('tests/fixtures/assets/nodejs.png', 'r', encoding='ISO-8859-1') as png:
+        return png.read()
+
+
+@pytest.fixture
+def resource_page_html():
+    """Return html in str format."""
+    with open('tests/fixtures/assets/courses.html', 'r') as html:
+        return html.read()
+
+
+def test_download_in_cwd(
+    source_page,
+    expected_page,
+    resource_page_png,
+    resource_page_css,
+    resource_page_html,
+):
     """Success download of a web page in cwd."""
     cwd = os.getcwd()
     with requests_mock.Mocker() as mock:
         mock.get(TEST_ADRESS, text=source_page)
+        mock.get(TEST_PNG, text=resource_page_png)
+        mock.get(TEST_CSS, text=resource_page_css)
+        mock.get(TEST_HTML, text=resource_page_html)
+
         with tempfile.TemporaryDirectory() as tmp:
             os.chdir(tmp)
-            download(TEST_ADRESS)
+            path = download(TEST_ADRESS)
+            files_path = path[:-5] + '_files'
             os.chdir(cwd)
-            with open(tmp + '/test1-test2-tes-t3-test4-tes-t5-com.html') as res:
+            with open(path) as res:
                 assert res.read() == expected_page
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-application.css',
+            )) as css:
+                assert css.read() == resource_page_css
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-courses.html',
+            )) as html:
+                assert html.read() == resource_page_html
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-nodejs.png',
+            )) as png:
+                assert png.read() == resource_page_png
 
 
-def test_download_cwd2(source_page, expected_page):
+def test_download_in_dir(
+    source_page,
+    expected_page,
+    resource_page_png,
+    resource_page_css,
+    resource_page_html,
+):
     """Success download of a web page in choosen dir."""
     with requests_mock.Mocker() as mock:
         mock.get(TEST_ADRESS, text=source_page)
+        mock.get(TEST_PNG, text=resource_page_png)
+        mock.get(TEST_CSS, text=resource_page_css)
+        mock.get(TEST_HTML, text=resource_page_html)
+
         with tempfile.TemporaryDirectory() as tmp:
-            download(TEST_ADRESS, output_path=tmp)
-            with open(tmp + '/test1-test2-tes-t3-test4-tes-t5-com.html') as res:
+            path = download(TEST_ADRESS, output_path=tmp)
+            files_path = path[:-5] + '_files'
+            with open(path) as res:
                 assert res.read() == expected_page
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-application.css',
+            )) as css:
+                assert css.read() == resource_page_css
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-courses.html',
+            )) as html:
+                assert html.read() == resource_page_html
+            with open(os.path.join(
+                files_path,
+                'mytest-com-caramba123-assets-nodejs.png',
+            )) as png:
+                assert png.read() == resource_page_png
 
 
 def test_download_nonexistent_res():
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -8,9 +8,11 @@ from page_loader.parsers import create_arg_parser, parse_file_name
 def test_parse_file_name():
     """Test parse file names."""
     url1 = 'https://ru.hexlet.io/courses'
-    url2 = 'https://www.google.ru/Ch#Eck'
+    url2 = 'https://www.google.ru/Ch=Eck'
+    url3 = 'https://test.ru/smth/file.js'
     assert parse_file_name(url1) == 'ru-hexlet-io-courses.html'
-    assert parse_file_name(url2) == 'google-ru-ch-eck.html'
+    assert parse_file_name(url2) == 'www-google-ru-ch-eck.html'
+    assert parse_file_name(url3, files=True) == 'test-ru-smth-file.js'
 
 
 def test_create_arg_parser():
